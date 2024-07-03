df887abecc82b4602f0d7f849b4b636281918733
andrewhector.cloete <andrewhector.cloete@vcontractor.co.za>
2024-07-03 16:59:39 +0200

    Updates home_citrix

--- home-manager/home_citrix.nix
+++ home-manager/home_citrix.nix
@@ -3,13 +3,17 @@ let
   params = {
     user = "user";
     homeDirectory = "/home/user";
+    dir_vulture = "${params.homeDirectory}/Workspace";
+
   };
 in
 {
   home.stateVersion = "23.11"; # Please read the comment before changing.
   home.sessionVariables = { };
 
-  home.packages = [
+  home.packages = with pkgs ; [
+    pkgs.maven
+    pkgs.jdk17
     pkgs.awscli2
     pkgs.dust
     pkgs.nodejs-18_x
@@ -20,6 +24,12 @@ in
     pkgs."kubernetes-helm"
     pkgs.glab
   ];
+
+  programs.zsh.shellAliases = {
+    "rts-env" = "source ${params.dir_vulture}/aws/rts/rts-lib/packages/vodacom_rts_cdk/scripts/rts-env.sh";
+    "rts-env-pip" = "source ${params.dir_vulture}/aws/rts/rts-lib/packages/vodacom_rts_cdk/scripts/rts-env-pip.sh";
+  };
+
 
   imports = [
     (import ./base.nix ({ inherit config pkgs params; }))
