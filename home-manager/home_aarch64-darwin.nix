1878111bca3dbe6ade3d27cd175bb8cb33c7a83f
Andrew Cloete <andrew.cloete@spatialedge.co.za>
2024-09-05 14:12:53 +0200

    Adds ccc alias

--- home-manager/home_aarch64-darwin.nix
+++ home-manager/home_aarch64-darwin.nix
@@ -63,6 +63,7 @@ in
     awsl = "${params.homeDirectory}/Vulture/aws/vodacom-aws-auth/.venv/bin/python ${params.homeDirectory}/Vulture/aws/vodacom-aws-auth/aws_auth.py";
     "rts-env" = "source ${params.dir_vulture}/aws/rts/rts-lib/packages/vodacom_rts_cdk/scripts/rts-env.sh";
     "rts-env-pip" = "source ${params.dir_vulture}/aws/rts/rts-lib/packages/vodacom_rts_cdk/scripts/rts-env-pip.sh";
+    "ccc" = "while [ $? -eq 0 ]; do echo Delete; security delete-internet-password -l git-codecommit.eu-west-1.amazonaws.com ; done";
   };
 
   imports = [
