a3b4efb2c1c5fb5f867be9aece44199c4eeee8e4
Andrew Cloete <andrew.cloete@spatialedge.co.za>
2025-12-26 06:35:16 +0200

    nb_book updates

--- home-manager/bin/nb_book.sh
+++ home-manager/bin/nb_book.sh
@@ -10,12 +10,15 @@ BOOK_DATA=$(curl "https://openlibrary.org/api/books?bibkeys=ISBN:${ISBN}&format=
 
 
 
+echo $BOOK_DATA | jq
+
 get_key() {
     echo $BOOK_DATA | jq -r ".[\"ISBN:${ISBN}\"].$1"
 }
 
 
 TITLE=$(get_key "title")
+SUBTITLE=$(get_key "subtitle")
 # The filter: wrap name in brackets, wrap that in escaped quotes, then join with commas
 AUTHORS=$(get_key "authors[] | \"- \\\"[[\" + .name + \"]]\\\"\"")
 DATE=$(get_key "publish_date")
@@ -31,6 +34,10 @@ ${AUTHORS}
 date: ${DATE}
 ISBN: ${ISBN}
 ---
+
+#Books
+Subtitle: ${SUBTITLE}
+
 """
 
 FILE="${BOOK_DIR}/${TITLE}.md"
